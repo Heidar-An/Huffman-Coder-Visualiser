8bf651fc670d08609f3053f3a4f655d965a9dffa
Heidar-An <74025356+Heidar-An@users.noreply.github.com>
2021-07-08 17:19:50 +0100

    Create NodeClass.js

---
+++ NodeClass.js
@@ -0,0 +1,12 @@
+class node{
+    constructor(freq, symbol, left = null, right = null, x = 0, y = 0){
+      
+      this.freq = freq;
+      this.symbol = symbol;
+      this.left = left;
+      this.right = right;
+      this.huff = '';
+      this.x = x;
+      this.y = y;
+    }
+}
